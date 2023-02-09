846c6fec62e23e06ba76a32b41070d14f555076d
Mohamed Osman <mohamedosman@Mohameds-MacBook-Pro.local>
2023-02-08 22:12:51 -0500

    Table of Contents within generateMarkdown.js

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,5 +1,6 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
+
 function renderLicenseBadge(license) {
   if (license !=="none") {
     return `![licensebadge](https://img.shields.io/badge/License-${license}-red)`
@@ -9,13 +10,16 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
+
 function renderLicenseLink(license) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
+
 function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
+
 function generateMarkdown(data) {
   return `# ${data.title}
 ${renderLicenseBadge(data.license)}
@@ -24,10 +28,36 @@ ${renderLicenseBadge(data.license)}
 
 ${data.description}
 
+## Table of Contents
+- [Installation](#installation)
+- [Usuage](#usage)
+- [Contribution](#contribution)
+- [Tests](#tests)
+- [License](#license)
+
+## Installation
+
+${date.installation}
+
+## Usage
+
+${data.usage}
+
+## Contribution
+
+${data.contribution}
+
+## Tests
+
+${data.tests}
+
+## License
+
+${renderLicenseSection(data.license)}
+
 
 `;
 }
 
 module.exports = generateMarkdown;
 
-//TABLE OF CONTENTS - rll(run renderlisencesection), rcs(section for readme)
